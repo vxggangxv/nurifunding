db6f61c2a71d4bc92ee7ea4d56bed9ec2bceb2a7
Hye jun <vxggangxv@naver.com>
2017-03-15 20:37:10 +0900

    대출신청페이지완료

--- src/js/components_custom.js
+++ src/js/components_custom.js
@@ -3,4 +3,19 @@ $(function() {
   $(".dropdown .btn").on("click", function() {
     $(this).next().slideToggle("fast");
   });
+  /*체크박스 전체동의*/
+  $(".nr-check.all").on("click", function() {
+    var isChecked = $(this).prop("checked");
+    //console.log(isChecked);
+    $(this).closest(".main-box").find(".nr-check").prop("checked", isChecked);
+  })
+  /*텝박스 기능*/
+  $(".tab-box li").on("click", function() {
+    var thIdx = $(this).index();
+    $(this).closest("ul").find("li").removeClass("on");
+    $(this).closest("ul").find("li").eq(thIdx).addClass("on");
+    $(this).closest("ul").next().find("li").removeClass("on");
+    $(this).closest("ul").next().find("li").eq(thIdx).addClass("on");
+  });
+  
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,5 @@
 $(function() {
-  /*모바일 바 클릭시 모바일 네비게이션 등장*/
-  $(".header .nav-tab").on("click", function() {
-    $("#mlnb-wrap").addClass("on");
-  });
-  /*모바일 네비게이션 닫기 버튼*/
-  $("#mlnb-wrap .cls-btn").on("click", function() {
-    $("#mlnb-wrap").removeClass("on");
-  });
+
+  
 
 });
